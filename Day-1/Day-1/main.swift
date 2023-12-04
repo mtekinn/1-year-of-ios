277da19411f6bda60c3f5dcf934917f26670e7ef
mtekinn <tekinmdev@gmail.com>
2023-12-04 19:54:36 +0300

    new changes

---
+++ Day-1/Day-1/main.swift
@@ -0,0 +1,43 @@
+//MARK: - Variables
+
+var name = "Mehmet"
+print(name) // Mehmet
+name = "Tekin"
+print(name) // Tekin
+
+var age = 25
+// age = "Tweenty Five" // cannot be changed
+
+
+// Multi-line Strings
+var multilineText = """
+This is a multiline test string
+and i'm learning Swift.
+Test Test Test Test
+Test Test Test Test
+"""
+// Doubles and Integers
+
+// Integer
+var myInt = 1
+myInt = 41
+myInt = 12
+
+// Double
+var myDouble = 1.0
+myDouble = 21.2562
+myDouble = 3.14
+
+// String Interpolation
+
+var number = 101
+print("My number is \(number)") // My number is 101
+
+// Type annotations
+
+var myNumber: Int = 12
+var myName: String = "Mehmet"
+var myBool: Bool = false
+var myDouble2: Double = 3.14
+
+// This line for test commit
