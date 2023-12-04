d3c05f8ffa7013e2ee0dd7e081d64853ad2feec5
mtekinn <tekinmdev@gmail.com>
2023-12-04 20:18:49 +0300

    initial commit

--- Day-1/Day-1/main.swift
+++ Day-1/Day-1/main.swift
@@ -1,43 +1,11 @@
-//MARK: - Variables
+//
+//  main.swift
+//  Day-1
+//
+//  Created by Mehmet Tekin on 4.12.2023.
+//
 
-var name = "Mehmet"
-print(name) // Mehmet
-name = "Tekin"
-print(name) // Tekin
+import Foundation
 
-var age = 25
-// age = "Tweenty Five" // cannot be changed
+print("Hello, World!")
 
-
-// Multi-line Strings
-var multilineText = """
-This is a multiline test string
-and i'm learning Swift.
-Test Test Test Test
-Test Test Test Test
-"""
-// Doubles and Integers
-
-// Integer
-var myInt = 1
-myInt = 41
-myInt = 12
-
-// Double
-var myDouble = 1.0
-myDouble = 21.2562
-myDouble = 3.14
-
-// String Interpolation
-
-var number = 101
-print("My number is \(number)") // My number is 101
-
-// Type annotations
-
-var myNumber: Int = 12
-var myName: String = "Mehmet"
-var myBool: Bool = false
-var myDouble2: Double = 3.14
-
-// This line for test commit
