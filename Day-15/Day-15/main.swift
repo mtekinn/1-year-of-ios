d062c23853f1059a657a8e1f80a041e8ec0cd0d0
mtekinn <tekinmdev@gmail.com>
2023-12-19 14:51:53 +0300

    Day 15 final commit

--- Day-15/Day-15/main.swift
+++ Day-15/Day-15/main.swift
@@ -1,4 +1,4 @@
-
+import SwiftUI
 //MARK: - Day 15 Summary 3
 
 // Topics
@@ -70,4 +70,120 @@ print(tri.perimeter)
 
 // 2. Static properties and methods
 
+struct PinkFloydFan {
+    static var favoriteSong = "Comfortably Numb"
+    
+    var name: String
+    var age: Int
+}
 
+let pinkFan = PinkFloydFan(name: "Mehmet", age: 24)
+print(PinkFloydFan.favoriteSong)
+
+
+
+// 3. Access control
+
+
+class PrivateName {
+    private var name: String?
+}
+
+let pName = PrivateName()
+// pName.name // 'name' is inaccessible due to 'private' protection level
+
+
+class FilePrivateClass {
+    fileprivate var number: Int
+    
+    init(number: Int) {
+        self.number = number
+    }
+}
+
+var fpClass = FilePrivateClass(number: 24)
+print(fpClass.number) // 24
+
+
+
+// 4. Polymorphism and typecasting
+
+
+class Car {
+    var brand: String
+    
+    init(brand: String) {
+        self.brand = brand
+    }
+    
+    func performance() {
+        print("\(self.brand) has good performance.")
+    }
+}
+
+
+class ClassicCar: Car {
+    var model: Int
+    
+    init(brand: String, model: Int) {
+        self.model = model
+        super.init(brand: brand)
+    }
+    
+    override func performance() {
+        print("\(self.brand) \(self.model) has an awesome design.")
+    }
+}
+
+
+class SportCar: Car {
+    var engineHP: Int
+    
+    init(brand: String, engineHP: Int) {
+        self.engineHP = engineHP
+        super.init(brand: brand)
+    }
+    
+    override func performance() {
+        print("\(self.brand) has \(self.engineHP)hp, that's why it's fast and strong.")
+    }
+}
+
+
+var car1 = Car(brand: "Tesla")
+var car2 = ClassicCar(brand: "Mercedes", model: 1972)
+var car3 = SportCar(brand: "Ferrari", engineHP: 300)
+
+var garage: [Car] = [car1, car2, car3]
+
+
+for car in garage {
+    if let classicCar = car as? ClassicCar {
+        classicCar.performance()
+    } else {
+        print("This is not classic car")
+    }
+}
+
+
+let number = 12
+let strNumber = String(number)
+print(type(of: strNumber)) // String
+
+
+
+// 5. Closures
+
+
+let message = "Button pressed"
+
+Button("Press Me") {
+    print(message)
+}
+
+
+Button {
+    print("The button was pressed")
+} label: {
+    Image("press-me")
+}
