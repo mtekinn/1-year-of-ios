652b70cb14600e8ae512fa36f2075d6553d4f0a7
Muhammet Tekin <tekinmdev@gmail.com>
2024-01-04 18:03:32 +0300

    DAY 33 Project 7, part one

---
+++ Day-33/Project7/Project7/Petition.swift
@@ -0,0 +1,15 @@
+//
+//  Petition.swift
+//  Project7
+//
+//  Created by Mehmet Tekin on 4.01.2024.
+//
+
+import Foundation
+
+
+struct Petition: Codable {
+    var title: String
+    var body: String
+    var signatureCount: Int
+}
---
+++ Day-33/Project7/Project7/Petitions.swift
@@ -0,0 +1,13 @@
+//
+//  Petitions.swift
+//  Project7
+//
+//  Created by Mehmet Tekin on 4.01.2024.
+//
+
+import Foundation
+
+
+struct Petitions: Codable {
+    var results: [Petition]
+}
