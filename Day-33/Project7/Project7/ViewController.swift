a3c88ef74483582d860b287d94533e7b25305981
Muhammet Tekin <tekinmdev@gmail.com>
2024-01-06 22:51:11 +0300

    DAY 35 Project 7, part three, hard wrap up check later

--- Day-33/Project7/Project7/ViewController.swift
+++ Day-33/Project7/Project7/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 class ViewController: UITableViewController {
 
     var petitions = [Petition]()
+    var filteredPetitions = [Petition]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,14 +18,32 @@ class ViewController: UITableViewController {
         title = "Whitehouse People API"
         
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Credits", style: .plain, target: self, action: #selector(showAlert))
-        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .edit, target: self, action: #selector(filterPetitions))
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Filter", style: .plain, target: self, action: #selector(filterPetitions))
 
         dataFromURL()
     }
     
-    @objc func filterPetitions(input: String) {
-        
+    @objc func filterPetitions() {
+        let ac = UIAlertController(title: "Search", message: "Enter a search string", preferredStyle: .alert)
+        ac.addTextField()
+        ac.addAction(UIAlertAction(title: "Filter", style: .default, handler: { [unowned ac, weak self] _ in
+            guard let searchTerm = ac.textFields?[0].text else { return }
+            self?.title = "\"\(searchTerm)\""
+            
+            DispatchQueue.global(qos: .userInitiated).async { [weak self] in
+                
+                self?.filteredPetitions = self?.petitions.filter { $0.title.uppercased().contains(searchTerm.uppercased()) || $0.body.uppercased().contains(searchTerm.uppercased())
+                    } ?? []
+                 
+                DispatchQueue.main.async {
+                    self?.tableView.reloadData()
+                }
+            }
+        }))
+                     
+        present(ac, animated: true)
     }
+    
     
     func dataFromURL() {
         let urlString: String
@@ -61,26 +80,28 @@ class ViewController: UITableViewController {
         let decoder = JSONDecoder()
 
         if let jsonPetitions = try? decoder.decode(Petitions.self, from: json) {
-            petitions = jsonPetitions.results
+            self.petitions = jsonPetitions.results
+            self.filteredPetitions = jsonPetitions.results
+            
             tableView.reloadData()
         }
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return petitions.count
+        return filteredPetitions.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        let petition = petitions[indexPath.row]
-        cell.textLabel?.text = petition.title.uppercased()
-        cell.detailTextLabel?.text = petition.body
+        cell.textLabel?.text = filteredPetitions[indexPath.row].title
+        cell.detailTextLabel?.text = filteredPetitions[indexPath.row].body
+        
         return cell
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let vc = DetailViewController()
-        vc.detailItem = petitions[indexPath.row]
+        vc.detailItem = filteredPetitions[indexPath.row]
         navigationController?.pushViewController(vc, animated: true)
     }
 }
