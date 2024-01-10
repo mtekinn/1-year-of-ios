29e2e3362e73b56beb0078dcac95cf0cc01179d4
Muhammet Tekin <tekinmdev@gmail.com>
2024-01-10 20:33:21 +0300

    DAY 42 Project 10, part one commit

---
+++ Day-42/Project10/Project10/PersonCell.swift
@@ -0,0 +1,14 @@
+//
+//  PersonCell.swift
+//  Project10
+//
+//  Created by Mehmet Tekin on 10.01.2024.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+    
+}
---
+++ Day-42/Project10/Project10/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Project10
+//
+//  Created by Mehmet Tekin on 10.01.2024.
+//
+
+import UIKit
+
+class ViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Person", for: indexPath) as? PersonCell else {
+            fatalError("Unable to dequeue PersonCell.")
+        }
+        return cell
+    }
+}
+
