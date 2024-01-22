cade8dec55c4b4967cee69faecd018a7475962f0
Muhammet Tekin <tekinmdev@gmail.com>
2024-01-22 14:10:55 +0300

    DAY 57 Project 15, initial commit

---
+++ Day-57/Project15/Project15/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project15
+//
+//  Created by Mehmet Tekin on 22.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
