7e1adf3e238c70dd5fbf9a969ce5b40d9c7d822d
Muhammet Tekin <tekinmdev@gmail.com>
2024-01-24 12:18:48 +0300

    DAY 60 Project 16, part one initial commit

---
+++ Day-60/Project16/Project16/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project16
+//
+//  Created by Mehmet Tekin on 24.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
