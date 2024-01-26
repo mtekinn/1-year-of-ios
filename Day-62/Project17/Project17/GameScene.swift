1cb790cac49c9cb5d1637be39824b26a4fe602ad
Muhammet Tekin <tekinmdev@gmail.com>
2024-01-26 12:47:27 +0300

    DAY 62 Project 17, initial commit

---
+++ Day-62/Project17/Project17/GameScene.swift
@@ -0,0 +1,19 @@
+//
+//  GameScene.swift
+//  Project17
+//
+//  Created by Mehmet Tekin on 26.01.2024.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+    
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+    }
+}
