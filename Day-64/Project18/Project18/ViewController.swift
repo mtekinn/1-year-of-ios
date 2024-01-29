8e727f16d9ab9b40a850cf281a2ac450780bd339
Muhammet Tekin <tekinmdev@gmail.com>
2024-01-29 14:22:57 +0300

    DAY 64 Project 18, part one commit

--- Day-64/Project18/Project18/ViewController.swift
+++ Day-64/Project18/Project18/ViewController.swift
@@ -11,7 +11,21 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+//        print("I'm inside the viewDidLoad() method!")
+//        print(1, 2, 3, 4, 5, separator: "*")
+//        print("Some message", terminator: " ... ")
+                
+//        assert(myReallySlowMethod() == true, "The slow method returned false, which is a bad thing!")
+        
+        for i in 1...100 {
+            print("Got number \(i)")
+        }
+    }
+    
+    func myReallySlowMethod() -> Bool {
+        print("This is inside the myReallySlowMethod methhod")
+        return true
     }
 
 
