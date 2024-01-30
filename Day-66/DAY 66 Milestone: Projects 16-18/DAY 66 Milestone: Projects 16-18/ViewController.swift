2255c2c9b6ce01b5e2de9d4bd7d0e9612659a28c
Muhammet Tekin <tekinmdev@gmail.com>
2024-01-30 12:28:37 +0300

    DAY 66 Milestone: Projects 16-18 initial commit

---
+++ Day-66/DAY 66 Milestone: Projects 16-18/DAY 66 Milestone: Projects 16-18/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DAY 66 Milestone: Projects 16-18
+//
+//  Created by Mehmet Tekin on 30.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
