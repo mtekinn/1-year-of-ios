e99ab6bb6e192b8aba05563ede166a94e0bc7b12
mtekinn <tekinmdev@gmail.com>
2024-02-08 23:08:36 +0300

    initial auth commit

--- Day-75/Project22/Project22/ViewController.swift
+++ Day-75/Project22/Project22/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         // test for configuration git
+        // test for auth
     }
 
 
