987dae22c9ce527bd413469be96509b40a4fe579
mtekinn <tekinmdev@gmail.com>
2024-02-10 13:53:30 +0300

    Project 23 initial commit

---
+++ Day-77/Project23/Project23/GameScene.swift
@@ -0,0 +1,21 @@
+//
+//  GameScene.swift
+//  Project23
+//
+//  Created by Mehmet Tekin on 10.02.2024.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+
+}
