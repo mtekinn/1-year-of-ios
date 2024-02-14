eb3a81896f4e39676bd13dcd3648a7e125e228ec
mtekinn <tekinmdev@gmail.com>
2024-02-14 20:02:22 +0300

    DAY 80 Project 24, part one commit

--- Day-80/Project24/Project24/main.swift
+++ Day-80/Project24/Project24/main.swift
@@ -6,10 +6,73 @@
 //
 
 import Foundation
+import SwiftUI
 
-let name = "Mehmet"
 
-for letter in name {
-    print("Give me a \(letter)")
+let password = "12345"
+// print(password.hasPrefix("123"))
+// print(password.hasSuffix("345"))
+
+extension String {
+    func deletingPrefix(_ prefix: String) -> String {
+        guard self.hasPrefix(prefix) else { return self }
+        return String(self.dropFirst(prefix.count))
+    }
+    
+    func deletingSuffix(_ suffix: String) -> String {
+        guard self.hasSuffix(suffix) else { return self }
+        return String(self.dropLast(suffix.count))
+    }
 }
 
+let weather = "it's going to rain"
+print(weather.capitalized)
+
+
+extension String {
+    var capitalizedFirst: String {
+        guard let firstLetter = self.first else { return "" }
+        return firstLetter.uppercased() + self.dropFirst()
+    }
+}
+
+
+print(weather.capitalizedFirst)
+
+
+let input = "Swift is like Objective-C without the C"
+print(input.contains("2"))
+
+let languages = ["Python", "Ruby", "Swift"]
+print(languages.contains("Swift"))
+
+
+extension String {
+    func containsAny(of array: [String]) -> Bool {
+        for item in array {
+            if self.contains(item) {
+                return true
+            }
+        }
+        return false
+    }
+}
+
+print(input.containsAny(of: languages))
+print(languages.contains(where: input.contains))
+
+
+let string = "This is a test string"
+let attributes: [NSAttributedString.Key: Any] = [
+    .foregroundColor: UIColor.white,
+    .backgroundColor: UIColor.red,
+    .font: UIFont.boldSystemFont(ofSize: 36)
+]
+
+let attributedString = NSAttributedString(string: string, attributes: attributes)
+let attributedString = NSMutableAttributedString(string: string)
+attributedString.addAttribute(.font, value: UIFont.systemFont(ofSize: 8), range: NSRange(location: 0, length: 4))
+attributedString.addAttribute(.font, value: UIFont.systemFont(ofSize: 16), range: NSRange(location: 5, length: 2))
+attributedString.addAttribute(.font, value: UIFont.systemFont(ofSize: 24), range: NSRange(location: 8, length: 1))
+attributedString.addAttribute(.font, value: UIFont.systemFont(ofSize: 32), range: NSRange(location: 10, length: 4))
+attributedString.addAttribute(.font, value: UIFont.systemFont(ofSize: 40), range: NSRange(location: 15, length: 6))
