5c519e7d6de4b2dbe8fb4ffe674e26dea517b870
mtekinn <tekinmdev@gmail.com>
2024-02-15 15:30:13 +0300

    DAY 81 Project 24 wrap up commit

--- Day-80/Project24/Project24/main.swift
+++ Day-80/Project24/Project24/main.swift
@@ -62,17 +62,45 @@ print(input.containsAny(of: languages))
 print(languages.contains(where: input.contains))
 
 
-let string = "This is a test string"
-let attributes: [NSAttributedString.Key: Any] = [
-    .foregroundColor: UIColor.white,
-    .backgroundColor: UIColor.red,
-    .font: UIFont.boldSystemFont(ofSize: 36)
-]
+extension String {
+    func withPrefix(_ str: String) -> String {
+        if self.contains(str) {
+            return self
+        } else {
+            return str + self
+        }
+    }
+}
 
-let attributedString = NSAttributedString(string: string, attributes: attributes)
-let attributedString = NSMutableAttributedString(string: string)
-attributedString.addAttribute(.font, value: UIFont.systemFont(ofSize: 8), range: NSRange(location: 0, length: 4))
-attributedString.addAttribute(.font, value: UIFont.systemFont(ofSize: 16), range: NSRange(location: 5, length: 2))
-attributedString.addAttribute(.font, value: UIFont.systemFont(ofSize: 24), range: NSRange(location: 8, length: 1))
-attributedString.addAttribute(.font, value: UIFont.systemFont(ofSize: 32), range: NSRange(location: 10, length: 4))
-attributedString.addAttribute(.font, value: UIFont.systemFont(ofSize: 40), range: NSRange(location: 15, length: 6))
+
+let a = "pet"
+
+print(a.withPrefix("car"))
+
+extension String {
+    func isNumeric() -> Bool {
+        for i in self {
+            if i.isNumber {
+                return true
+            }
+        }
+        return false
+    }
+}
+
+var myStr = "abc1def"
+print(myStr.isNumeric())
+
+extension String {
+    func lines() -> Array<String> {
+        var arr = [String]()
+        for i in self.split(separator: "\n") {
+            arr.append(String(i))
+        }
+        return arr
+    }
+}
+
+
+var myString = "this\nis\na\ntest"
+print(myString.lines())
