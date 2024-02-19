62ec3ebd44c37ac2434d2bdb25829d279d10986b
Muhammet Tekin <tekinmdev@gmail.com>
2024-02-19 18:31:07 +0300

    Milestone: Projects 1  final commit

--- Day-23/FlagApp/FlagApp/DetailViewController.swift
+++ Day-23/FlagApp/FlagApp/DetailViewController.swift
@@ -8,36 +8,41 @@
 import UIKit
 
 class DetailViewController: UIViewController {
-
-    @IBOutlet var selectedImage: UIImageView!
+    @IBOutlet var imageView: UIImageView!
     
+    var countryImage: String?
     var imageName: String?
-    
+    var imageNumber = 0
+    var totalImageNumber = 0
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        if let imageToLoad = imageName {
-            selectedImage.image = UIImage(named: imageToLoad)
-        }
-        setupUI()
-    }
-    
-    func setupUI() {
-        navigationController?.navigationBar.topItem?.title = "Fun with Flags"
-        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
-        selectedImage.layer.borderWidth = 1
-        selectedImage.layer.borderColor = UIColor.lightGray.cgColor
-    }
-    
-    @objc func shareTapped() {
-        guard let image = selectedImage.image?.jpegData(compressionQuality: 0.8) else {
-            print("No image Found")
-            return
-        }
         
-        let vc = UIActivityViewController(activityItems: [image], applicationActivities: [])
-        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
-        present(vc, animated: true)
+        title = "\(imageNumber + 1)/\(totalImageNumber)"
+        navigationItem.largeTitleDisplayMode = .never
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareAboutApp))
+        
+        if let imageToLoad = countryImage {
+            imageView.image = UIImage(named: imageToLoad)
+        }
     }
-
+    
+    
+    @objc func shareAboutApp() {
+        guard let image = imageView.image?.jpegData(compressionQuality: 0.9) else { return }
+        
+        let acv = UIActivityViewController(activityItems: [image, imageName!], applicationActivities: nil)
+        acv.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(acv, animated: true)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.hidesBarsOnTap = false
+    }
 }
-
--- Day-23/FlagApp/FlagApp/ViewController.swift
+++ Day-23/FlagApp/FlagApp/ViewController.swift
@@ -8,53 +8,52 @@
 
 import UIKit
 
-class TableViewController: UITableViewController {
-
-    var flags = [String]()
+class ViewController: UITableViewController {
     
+    var pictures = [String]()
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        title = "Share Flag Application"
         navigationController?.navigationBar.prefersLargeTitles = true
         
+        loadImages()
+    }
+    
+    func loadImages() {
         let fm = FileManager.default
         let path = Bundle.main.resourcePath!
         let items = try! fm.contentsOfDirectory(atPath: path)
         
-        for item in items {
-            if item.hasSuffix("3x.png") {
-                flags.append(item)
+        for item in items.sorted() {
+            if item.hasPrefix("nssl") {
+                pictures.append(item)
             }
         }
-        
     }
 
-    // MARK: - Table view data source
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return flags.count
+        return pictures.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        cell.textLabel?.text = flags[indexPath.row].uppercased().replacingOccurrences(of: "@3X.PNG", with: "")
-        cell.imageView?.image = UIImage(named: flags[indexPath.row])
-        
-        cell.imageView?.layer.borderWidth = 1
-        cell.imageView?.layer.borderColor = UIColor.lightGray.cgColor
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Country", for: indexPath)
+        var content = cell.defaultContentConfiguration()
+        content.text = pictures[indexPath.row]
+        cell.contentConfiguration = content
         return cell
-    }
-    
-    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 80
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
-            let index = indexPath.row
-            vc.imageName = flags[index]
+            vc.countryImage = pictures[indexPath.row]
+            vc.imageName = pictures[indexPath.row]
+            vc.imageNumber = indexPath.row
+            vc.totalImageNumber = pictures.count
             navigationController?.pushViewController(vc, animated: true)
-            vc.title = "Flag of \(flags[index].uppercased().replacingOccurrences(of: "@3X.PNG", with: ""))"
         }
     }
-   
+
 }
+
