9537b7f7c519ed77ec592fbb9f041275efa05fe9
Muhammet Tekin <tekinmdev@gmail.com>
2024-02-25 11:12:32 +0300

    DAY 93 Project 28, part two wrapup commits

--- Day-92/Project28/Project28/ViewController.swift
+++ Day-92/Project28/Project28/ViewController.swift
@@ -16,7 +16,10 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         title = "Nothing to see here"
-        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(saveSecretMessage))
+        navigationItem.rightBarButtonItem = nil
+
+
         let notificationCenter = NotificationCenter.default
         notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillHideNotification, object: nil)
         notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
@@ -60,6 +63,31 @@ class ViewController: UIViewController {
         secret.isHidden = true
         title = "Nothing to see here"
     }
+    
+    func unlockWithPassword(_ action: UIAlertAction? = nil) {
+        let passwordAc = UIAlertController(title: "Enter Your password", message: nil, preferredStyle: .alert)
+        passwordAc.addTextField()
+
+        let submitAction = UIAlertAction(title: "Submit", style: .default){ [weak self, weak passwordAc] _ in
+            guard let field = passwordAc?.textFields?[0].text else { return }
+            self?.checkPassword(password: field)
+        }
+
+        passwordAc.addAction(submitAction)
+        present(passwordAc, animated: true)
+    }
+    
+    func checkPassword(password: String) {
+        let appPassword = KeychainWrapper.standard.string(forKey: "Password") ?? ""
+        if password == appPassword {
+            unlockSecretMessage()
+        } else {
+            let errorAc = UIAlertController(title: "Wrong password", message: nil, preferredStyle: .alert)
+            errorAc.addAction(UIAlertAction(title: "Try Again!", style: .default, handler: unlockWithPassword))
+            errorAc.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+            present(errorAc, animated: true)
+        }
+    }
 
     @IBAction func authenticateTapped(_ sender: Any) {
         let context = LAContext()
@@ -75,8 +103,9 @@ class ViewController: UIViewController {
                     if success {
                         self?.unlockSecretMessage()
                     } else {
-                        let ac = UIAlertController(title: "Authentication failed", message: "You could not be verified; please try again.", preferredStyle: .alert)
-                        ac.addAction(UIAlertAction(title: "OK", style: .default))
+                        let ac = UIAlertController(title: "Login unsuccessful!", message: "You should use password to login!", preferredStyle: .alert)
+                        ac.addAction(UIAlertAction(title: "Use password", style: .default, handler: self?.unlockWithPassword))
+                        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
                         self?.present(ac, animated: true)
                     }
                 }
