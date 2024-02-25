e554df9c8f0ac3e27178f24fecb475b93879157f
Muhammet Tekin <tekinmdev@gmail.com>
2024-02-25 11:18:44 +0300

    Project 29 initial commit

---
+++ Day-94/Project29/Project29/GameScene.swift
@@ -0,0 +1,14 @@
+//
+//  GameScene.swift
+//  Project29
+//
+//  Created by Mehmet Tekin on 25.02.2024.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    override func didMove(to view: SKView) {
+        
+    }
+}
