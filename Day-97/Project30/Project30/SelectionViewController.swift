00295d0c452368b9eb3921342fc69931db3b3bd9
Muhammet Tekin <tekinmdev@gmail.com>
2024-03-01 01:33:09 +0300

    Project 30 wrap up commit

--- Day-97/Project30/Project30/SelectionViewController.swift
+++ Day-97/Project30/Project30/SelectionViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class SelectionViewController: UITableViewController {
 	var items = [String]() // this is the array that will store the filenames to load
-	var viewControllers = [UIViewController]() // create a cache of the detail view controllers for faster loading
+	
 	var dirty = false
 
     override func viewDidLoad() {
@@ -104,8 +104,6 @@ class SelectionViewController: UITableViewController {
 		// mark us as not needing a counter reload when we return
 		dirty = false
 
-		// add to our view controller cache and show
-		viewControllers.append(vc)
 		navigationController!.pushViewController(vc, animated: true)
 	}
 }
