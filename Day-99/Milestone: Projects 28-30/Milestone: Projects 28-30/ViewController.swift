cfe175e11669f463b1c733d5d672534c6f080a3b
Muhammet Tekin <tekinmdev@gmail.com>
2024-03-01 01:34:56 +0300

    DAY 99 Milestone: Projects 28-30 initial commit

---
+++ Day-99/Milestone: Projects 28-30/Milestone: Projects 28-30/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Milestone: Projects 28-30
+//
+//  Created by Mehmet Tekin on 1.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
